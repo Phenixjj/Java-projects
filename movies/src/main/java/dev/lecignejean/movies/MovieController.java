66d19aad182e5ec69e04b7aa20b70a768e8d94b7
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-04-02 16:13:09 +0200

    doc: add docs and comments for movieController class

--- movies/src/main/java/dev/lecignejean/movies/MovieController.java
+++ movies/src/main/java/dev/lecignejean/movies/MovieController.java
@@ -11,21 +11,42 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * This is a Rest Controller for managing movies.
+ * It provides endpoints to get all movies, get a movie by its id and get a movie by its IMDB id.
+ */
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+
+    // MovieService is used to interact with the database.
     @Autowired
     private MovieService movieService;
+
+    /**
+     * This method is used to get all the movies.
+     * @return ResponseEntity<List<Movie>> This returns a list of all movies with HTTP status code.
+     */
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
+    /**
+     * This method is used to get a movie by its id.
+     * @param id This is the id of the movie to be fetched.
+     * @return ResponseEntity<Optional<Movie>> This returns the movie with the given id with HTTP status code.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable ObjectId id) {
         return new ResponseEntity<Optional<Movie>>(movieService.getMovieById(id), HttpStatus.OK);
     }
 
+    /**
+     * This method is used to get a movie by its IMDB id.
+     * @param imdbId This is the IMDB id of the movie to be fetched.
+     * @return ResponseEntity<Optional<Movie>> This returns the movie with the given IMDB id with HTTP status code.
+     */
     @GetMapping("/imdb/{imdbId}")
     public ResponseEntity<Optional<Movie>> getMovieByImdbId(@PathVariable String imdbId) {
         return new ResponseEntity<Optional<Movie>>(movieService.getMovieByImbId(imdbId), HttpStatus.OK);
