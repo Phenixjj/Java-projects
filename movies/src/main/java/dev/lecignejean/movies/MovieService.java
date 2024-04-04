cc8f9b58d2b9c228455d2b87e0f8fae3faac70e1
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-04-04 14:00:39 +0200

    doc: add docs and comments for MovieService

--- movies/src/main/java/dev/lecignejean/movies/MovieService.java
+++ movies/src/main/java/dev/lecignejean/movies/MovieService.java
@@ -6,19 +6,39 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * This service class is responsible for handling operations related to movies.
+ */
 @Service
 public class MovieService {
-        @Autowired
-        private MovieRepository movieRepository;
-        public List<Movie> allMovies() {
-            return movieRepository.findAll();
-        }
 
-        public Optional<Movie> getMovieById(ObjectId id){
-            return movieRepository.findById(id);
-        }
+    // MovieRepository instance for interacting with the database
+    @Autowired
+    private MovieRepository movieRepository;
 
-        public Optional<Movie> getMovieByImbId(String imdbId){
-            return movieRepository.findByImdbId(imdbId);
-        }
+    /**
+     * This method is used to fetch all the movies from the database.
+     * @return List<Movie> This returns a list of all movies.
+     */
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    /**
+     * This method is used to fetch a movie by its ObjectId.
+     * @param id This is the id of the movie to be fetched.
+     * @return Optional<Movie> This returns an Optional that contains the movie if it exists, else returns an empty Optional.
+     */
+    public Optional<Movie> getMovieById(ObjectId id){
+        return movieRepository.findById(id);
+    }
+
+    /**
+     * This method is used to fetch a movie by its IMDB id.
+     * @param imdbId This is the IMDB id of the movie to be fetched.
+     * @return Optional<Movie> This returns an Optional that contains the movie if it exists, else returns an empty Optional.
+     */
+    public Optional<Movie> getMovieByImbId(String imdbId){
+        return movieRepository.findByImdbId(imdbId);
+    }
 }
