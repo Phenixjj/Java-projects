88b062d82ebf68bfc048126353f5b1eeb2824879
Phenixjj <58008493+Phenixjj@users.noreply.github.com>
2024-04-24 16:03:56 +0200

    doc: add docs and comments for WebConfig

--- movies/src/main/java/dev/lecignejean/movies/WebConfig.java
+++ movies/src/main/java/dev/lecignejean/movies/WebConfig.java
@@ -5,9 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * This is a configuration class for the web application.
+ * It implements the WebMvcConfigurer interface to customize the Java-based configuration for Spring MVC.
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * This method is used to add CORS mappings.
+     * It allows requests from the specified origins to all endpoints ("/**").
+     * It also allows all HTTP methods and headers, and supports credentials.
+     * @param registry This is the CorsRegistry to which the mappings are added.
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
